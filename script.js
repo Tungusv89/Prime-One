adf79d49876fedafd657ae54f11dd61f256858d0
Evgeny <tungusv89@mail.ru>
2021-08-08 20:29:49 +0500

    первый блок готов

--- script.js
+++ script.js
@@ -53,3 +53,21 @@ $('.header-menu__icon').click(function (event) {
             $('body,html').scrollTop(parseInt($('body').data('scroll')))
         }
 });
+
+//ibg
+
+function ibg() {
+    $.each($('.ibg'), function (index, val) {
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+ibg();
+
+//Клик вне области
+$(document).on('click touchstart', function (e) {
+    if (!$(e.target).is(".select *")) {
+        $('.select').removeClass('active');
+    }
+});
